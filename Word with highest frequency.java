78282f95a6c3581736b62bef11c0b4bf7e33f78c
Yamuna B <yamunajeevana634@gmail.com>
2026-08-07 12:21:31 +0530

    Update Word with highest frequency.java

--- Word with highest frequency.java
+++ Word with highest frequency.java
@@ -1,1 +1,21 @@
-
+import java.util.*;
+class HiheghestFrequency{
+  public static void main (String[] args){
+    String[] str = " Java is easy Jav is easy Java Java";
+    String word = str.spilt(" ");
+    HaashMap<String, Integer> map = new HashMap<>();
+    for(String words : word){
+      map.put(word, map.getOrDefault(word, 0)+1);
+    }
+    String maxword = " ";
+    int maxcount = 0;
+    for(Map.Entry<String, Integer> entry : map.entrySet()){
+      if(entry.getValue()>maxcount){
+        maxcount = entry.getValue();
+        maxword - entry.getKey();
+      }
+    System.out.print("Highest frequency word is : " + maxword);
+    System.out.print("count is " + maxcount);
+    }
+  }
+}
