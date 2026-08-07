cc342992aa88f131ad5530c9d3b971aac97caac2
Yamuna B <yamunajeevana634@gmail.com>
2026-08-07 12:46:27 +0530

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,1 +1,12 @@
-
+import java.util.*;
+class RemoveDuplicate{
+  public static void main (String [] args){
+    int [] arr = { 50,78,39,39,49,50,18};
+    HashSet <Integer> set =  new HashSet<>():
+      for (int num : arr){
+      set.add(num);
+    }
+    System.out.print("Unique elements : " + set);
+  }
+}
+}
