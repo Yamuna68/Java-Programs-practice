49400922bb58afec3c344dad2833a39267886486
Yamuna B <yamunajeevana634@gmail.com>
2026-08-07 12:36:51 +0530

    Implement logic to find second highest element
    Fix variable naming for second highest element logic.

--- SecondHighestElement.java
+++ SecondHighestElement.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+class SecondHighest{
+  public static void main (String[] args){
+    int [] arr = {10,40,80,20,60,69,98};
+    int first = Integer.MIN_VALUE;
+    int Second = Integer.MIN_VALUE;
+    for(int num : arr){
+      if(num > first){
+        second = first;
+        first = num;
+      }else if(num >second && num!=first){
+        second = num;
+      }
+    }
+    System.out.println("Second largest element is : " + second);
+  }
+}
+}
+    
