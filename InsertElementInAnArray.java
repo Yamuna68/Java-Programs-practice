850ce69424c1d5a58d2a7c3a7fe09b0f047f4a7f
Yamuna B <yamunajeevana634@gmail.com>
2026-08-10 12:26:28 +0530

    Add element insertion logic in InsertElement class
    Implemented a method to insert an element into an array at a specified position and print the new array.

--- InsertElementInAnArray.java
+++ InsertElementInAnArray.java
@@ -1,1 +1,22 @@
-
+class InsertElement {
+    public static void main(String[] args) {
+        int [] arr ={10, 20, 30, 40, 50};
+        int position = 4;
+        int element = 55;
+        
+        int[] newArr = new int[arr.length + 1];
+        for (int i = 0 ; i<newArr.length ; i++){
+            if(i < position){
+                newArr[i] = arr[i];
+            }else if (i == position){
+                newArr[i] = element;
+            }else{
+                newArr[i] = arr[i -1];
+            }
+        }
+        for(int i = 0 ; i < newArr.length ; i++){
+            System.out.print(newArr[i] + " ");
+        }
+        
+    }
+}
