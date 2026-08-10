d3b281f3a1612456ba37fd41925d1e7f6447a1a7
Yamuna B <yamunajeevana634@gmail.com>
2026-08-10 12:57:21 +0530

    Implement element insertion for dynamic array size
    Added functionality to insert an element into an array with a dynamic size.

--- InsertElementInAnArray.java
+++ InsertElementInAnArray.java
@@ -1,3 +1,5 @@
+//insert the element if the size is fixed
+
 class InsertElement {
     public static void main(String[] args) {
         int [] arr ={10, 20, 30, 40, 50};
@@ -20,3 +22,30 @@ class InsertElement {
         
     }
 }
+
+// insert the element if the size is not fixed
+class InsertElement {
+    public static void main(String[] args) {
+        int [] arr= new int [6];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2]= 30;
+        arr[3]= 40;
+        arr[4]= 50;
+        
+        int size =5;
+        int position = 5;
+        int element = 35;
+        
+        for(int i=4 ; i>=position ; i--){
+            arr[i+1] = arr[i];
+        }
+        arr[position] = element;
+        size++;
+        for(int i=0 ; i<size ; i++){
+        System.out.print(arr[i] + " ");
+        }
+       
+    }
+}
+
