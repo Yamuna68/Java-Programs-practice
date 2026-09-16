61e74d5b94abdf5b48ab8ea3a79ea5038629f3cd
Yamuna B <yamunajeevana634@gmail.com>
2026-09-16 10:42:13 +0530

    Add Java program to reverse a number

---
+++ Reverse a number.java
@@ -0,0 +1,14 @@
+import java.Util.*;
+class ReverseNum{
+  public static void main(String [] args){
+    int num = 1234;
+    int rev = 0;
+    int orgnum = num;
+    while(num!=0){
+      int digit = num % 10;
+      rev = rev * 10 + digit;
+      num = num/10;
+    }
+    System.out.print(rev);
+  }
+}
