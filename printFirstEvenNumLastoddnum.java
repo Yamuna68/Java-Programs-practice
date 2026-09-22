bb01d10517c26f8057f02540cedf044f136dd2a0
Yamuna B <yamunajeevana634@gmail.com>
2026-09-22 15:43:50 +0530

    Add program to separate and print even and odd numbers
    Implement a program to print first even numbers followed by last odd numbers from an array.

---
+++ printFirstEvenNumLastoddnum.java
@@ -0,0 +1,23 @@
+class FirstEvenLastOdd {
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3, 4, 5, 6, 7};
+        int [] result = new int[arr.length] ;
+        int index = 0;
+        for(int i =0 ; i<arr.length; i++){
+            if(arr[i] %2 ==0){
+                result[index] = arr[i];
+                index++;
+            }
+        }
+        for(int i=0 ; i<arr.length; i++){
+            if(arr[i] %2!=0){
+                result[index]=arr[i];
+                index++;
+            }
+        }
+        for(int i=0 ; i<result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+       
+    }
+}
